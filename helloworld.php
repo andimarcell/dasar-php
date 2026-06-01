7209472043c1a0a5826955cd7ea1d10e13ab7b6f
Andi <andimarcell0064@gmail.com>
2026-06-02 01:02:35 +0800

    Komentar

--- helloworld.php
+++ helloworld.php
@@ -1,2 +1,8 @@
 <?php
+// This is a simple PHP script that outputs a greeting message.
+/*
+echo "Hello, <h1>Kelas Full Stack Web Developer!</h1>";
+echo "Hello, <h1>Kelas Full Stack Web Developer!</h1>";
+echo "Hello, <h1>Kelas Full Stack Web Developer!</h1>";
+*/
 echo "Hello, <h1>Kelas Full Stack Web Developer!</h1>";
