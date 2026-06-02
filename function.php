3e7188e7ad73423ed367e0f3c598a09484c4031a
Andi <andimarcell0064@gmail.com>
2026-06-02 17:43:39 +0800

    Function

---
+++ function.php
@@ -0,0 +1,27 @@
+<?php
+function penjumlahan()
+{
+    /*
+    $nama = "John Doe";
+    $umur = 22;
+    echo "namanya adalah $nama, dan saya berumur $umur tahun dan 5 tahun lagi umurnya adalah", $umur + 5;
+    */
+
+    $a = 10;
+    $b = 20;
+    echo "Hasil penjumlahan $a + $b = ", $a + $b;
+}
+function pengurangan()
+{
+    $a = 20;
+    $b = 10;
+    echo "Hasil pengurangan $a - $b = ", $a - $b;
+}
+
+pengurangan();
+penjumlahan();
+/*
+penjumlahan();
+penjumlahan();
+penjumlahan();
+*/
