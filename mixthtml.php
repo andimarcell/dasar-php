7d9631f3d67a9cb6ecc35a0b054b8f3bd27a8f74
Andi <andimarcell0064@gmail.com>
2026-06-02 17:01:08 +0800

    Mix HTML

---
+++ mixthtml.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mix HTML</title>
+</head>
+
+<body>
+    <h1>Hello world</h1><br>
+    <h2 style="color: red;">
+        <?php
+        $nama = "John Doe";
+        $umur = 22;
+        echo "namanya adalah $nama, dan saya berumur $umur tahun.",
+        50 * 50;
+        ?>
+    </h2>
+</body>
+
+</html>
