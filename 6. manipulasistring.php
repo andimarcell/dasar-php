0216cf1f6bed98b521e7f81b8fdff92f49379d64
Andi <andimarcell0064@gmail.com>
2026-09-18 22:49:10 +0800

    feat (add): menambahkan penjelasan lebih jelas di file manipulasi string

--- 6. manipulasistring.php
+++ 6. manipulasistring.php
@@ -4,18 +4,18 @@ $nama = "John Doe"; // string
 // manipulasi string
 $text = "belajar php itu menyenangkan";
 
-echo strLen($text); // panjang karakter
-
-echo strtoupper($text); // mengubah menjadi huruf besar
-
-echo strtolower($text); // mengubah menjadi huruf kecil
-
-echo ucfirst($text); // mengubah huruf pertama menjadi kapital
-
-echo ucwords($text); // mengubah huruf pertama setiap kata menjadi kapital
-
-echo strrev($text); // membalikkan string
-
-echo str_replace("menyenangkan", "CODEPOLITAN", $text); // mengganti kata tertentu dengan kata lain
-
-echo str_repeat($text, 2); // mengulangi string sebanyak 2 kali
+echo "panjang karakter: " . strlen($text) . "<br>";
+echo "<br>";
+echo "Huruf besar: " . strtoupper($text) . "<br>";
+echo "<br>";
+echo "Huruf kecil: " . strtolower($text) . "<br>";
+echo "<br>";
+echo "Huruf pertama kapital: " . ucfirst($text) . "<br>";
+echo "<br>";
+echo "Huruf pertama setiap kata kapital: " . ucwords($text) . "<br>";
+echo "<br>";
+echo "String dibalik: " . strrev($text) . "<br>";
+echo "<br>";
+echo "String setelah penggantian: " . str_replace("menyenangkan", "CODEPOLITAN", $text) . "<br>";
+echo "<br>";
+echo "String diulangi: " . str_repeat($text, 2) . "<br>"; // mengulangi string sebanyak 2 kali
