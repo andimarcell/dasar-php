157e61fab2cd7f150e8dc71a803acb616a498767
Andi <andimarcell0064@gmail.com>
2026-09-21 22:40:44 +0800

    null dan boolean

---
+++ 7. nulldanboolean.php
@@ -0,0 +1,28 @@
+<?php
+
+// null
+$hasil = null; // null = kosong
+
+$a = 1;
+$b = 5;
+$hasil = $a+$b;
+
+// echo $nama;
+echo $hasil;
+
+// boolean
+$nama = false; // false
+
+if ($nama) {
+    echo "namanya  salah";
+} else {
+    echo  "namanya benar";
+}
+
+$isLogin = true;
+
+if($isLogin) {
+    echo "selamat datang";
+} else {
+    echo "silahkan login";
+}
