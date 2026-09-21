eb1bae5e547a3c2ae3767b7e5ee82c9e7f813f53
Andi <andimarcell0064@gmail.com>
2026-09-21 22:47:08 +0800

    array

---
+++ 8. array.php
@@ -0,0 +1,15 @@
+<?php
+
+$buah = [
+    "apel",
+    "jeruk",
+    "mangga",
+    "pisang",
+    500,
+    1000,
+    true,
+    4.5,
+    null
+];
+
+echo $buah[0] . ", " . $buah[1] . ", " . $buah[2] . ", " . $buah[3];
