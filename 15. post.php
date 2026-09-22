c2e9d02a7ae91fdbab0d62ee5fd84dcd4b31337b
Andi <andimarcell0064@gmail.com>
2026-09-23 01:53:02 +0800

    globalVariablePOST

---
+++ 15. post.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FORM BIODATA</title>
+</head>
+<body>
+    <h2>Form Biodata</h2>
+    <form action="15. proses.php" method="POST">
+        <input type="text" name="nama" placeholder="Nama">
+        <input type="text" name="alamat" placeholder="Alamat">
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
